8815ccff5ba12c37780ced2129d05492f3edadf6
NetSe24 <netse0962@gmail.com>
2023-06-07 10:14:18 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderComponents />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <!-- <ul v-for="teacher of teachers" :key="teacher.name">
+    <li>{{ teacher.name }}</li>
+  </ul> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponents from "./components/HeaderComponents.vue";
 export default {
-  name: 'App',
+  name: "App",
+
+  data: function () {
+    return {
+      // title: "Hello World From Cambodia",
+      // teachers: [
+      //   { name: "Him" },
+      //   { name: "Yon" },
+      //   { name: "Rady" },
+      //   { name: "Ronan" },
+      //   { name: "Gareth" },
+      //   { name: "Tanguy" },
+      //   { name: "Mengheang" },
+      // ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    HeaderComponents,
+  },
+};
 </script>
 
-<style>
+<style >
+* {
+  /* padding: ; */
+  margin:0px;
+  font-family: verdana;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+ul > li {
+  color: red;
+  list-style-type: none;
 }
 </style>
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,82 @@
+<template>
+  <nav>
+    <div class="head-left">
+      <div class="group-logo">
+        <img src="../assets/logo.png" alt="" class="logo" />
+        <h1 class="logo-name">Vue.NET</h1>
+      </div>
+      <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+    </div>
+    <div class="head-right">
+      <div class="btn-group">
+        <a href="/post">
+          <button class="login">LOGIN</button>
+        </a>
+        <button class="register">REGISTER</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+
+<script >
+export default {
+  name: "HeaderComponents",
+};
+</script>
+
+<style scoped>
+template {
+  padding: 0;
+  margin: 0;
+}
+
+nav {
+  background: #000;
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.head-left {
+  padding: 10px;
+  display: flex;
+  align-items: center;
+}
+.group-logo {
+  display: flex;
+}
+.logo {
+  padding-right: 5%;
+  width: 16%;
+  height: 100%;
+}
+.logo-name {
+  color: #fff;
+}
+ul {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  /* color: #4fc08d; */
+}
+
+ul > li > a {
+  list-style: none;
+  text-decoration: none;
+  color: #ffffff;
+}
+button {
+  padding: 10px;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+  margin: 2px;
+}
+</style>
